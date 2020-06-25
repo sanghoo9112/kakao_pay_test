d8200e319db34c5249e25a1c0b7b21d7409c070a
derek <sanghoo9112@waug.com>
2020-06-25 14:24:51 +0900

    feat : 커밋 init:

---
+++ src/test/java/com/derekpark/balance/BalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derekpark.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
